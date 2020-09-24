a45e831f33b7dce200918521d951867127d87bed
PremJ16 <69350786+PremJ16@users.noreply.github.com>
2020-09-24 15:09:03 +0530

    sharedpreferences added

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -13,10 +14,11 @@ public class MainActivity extends AppCompatActivity {
     private TextView backgroundcounterTextView;
     private int buttoncount;
     private int backgroundcount;
-    static final String Button_count_key ="button_count_key";//key for savinginstances
-    static final String Backgrount_count_key ="background_count_key";//key for savinginstances
-    static final String Log_I_key="log_I_key";//created log for own understanding
-
+    static final String Button_count_key = "button_count_key";//key for savinginstances
+    static final String Backgrount_count_key = "background_count_key";//key for savinginstances
+    static final String Log_I_key = "log_I_key";//created log for own understanding
+    private SharedPreferences sharedPreferences;
+    static final String sharedPreferencesPath = "com.example.android.myapplication";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,20 +26,29 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.i(Log_I_key, "Inside onCreate");
 
-        this.buttoncount=0;//intialized to zero
-        this.backgroundcount=0;//intialized to zero
+        this.sharedPreferences = getSharedPreferences(sharedPreferencesPath, MODE_PRIVATE);
 
-        this.buttoncounterTextView=(TextView) findViewById(R.id.buttoncounter);//feeding the value
-        this.backgroundcounterTextView=(TextView) findViewById(R.id.backgroundcounter);
+        this.buttoncount = this.sharedPreferences.getInt(Button_count_key, 0);
 
+        this.backgroundcount = this.sharedPreferences.getInt(Backgrount_count_key, 0);
 
+        this.buttoncounterTextView = (TextView) findViewById(R.id.buttoncounter);//feeding the value
+        this.backgroundcounterTextView = (TextView) findViewById(R.id.backgroundcounter);
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        SharedPreferences.Editor sharedPreferencesEditor = this.sharedPreferences.edit();
+        sharedPreferencesEditor.putInt(Button_count_key, buttoncount);
+        sharedPreferencesEditor.putInt(Backgrount_count_key, backgroundcount);
+        sharedPreferencesEditor.apply();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(Log_I_key,"Inside onResume()");
+        Log.i(Log_I_key, "Inside onResume()");
         this.backgroundcounterTextView.setText(Integer.toString(this.backgroundcount));
         this.buttoncounterTextView.setText(Integer.toString(this.buttoncount));
 
@@ -45,30 +56,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onStop() {
-        Log.i(Log_I_key,"Inside onStop()");
+        Log.i(Log_I_key, "Inside onStop()");
         super.onStop();
         backgroundcount++;
 
+
     }
 
-    @Override
-    protected void onSaveInstanceState(@NonNull Bundle outState) {
-        super.onSaveInstanceState(outState);
-        Log.i(Log_I_key,"Inside onSaveInstances()");
-        outState.putInt(Button_count_key,this.buttoncount );
-        outState.putInt(Backgrount_count_key,backgroundcount-1);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
-        Log.i(Log_I_key,"Inside onRestoreInstances()");
-        this.buttoncount=savedInstanceState.getInt(Button_count_key);
-        this.backgroundcount=savedInstanceState.getInt(Backgrount_count_key);
-    }
 
     public void onbuttonclick(View view) {
-        Log.i(Log_I_key,"Inside onbuttonclick pressed()");
+        Log.i(Log_I_key, "Inside onbuttonclick pressed()");
         buttoncount++;
         this.buttoncounterTextView.setText(Integer.toString(this.buttoncount));
-    }}
+    }
+}
